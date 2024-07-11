2aad9df45af3cf18951fe4acce4f8052e2f17efb
shiki-01 <0110shiki@gmail.com>
2024-07-11 18:42:03 +0900

    save

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,34 @@
 <script>
-	import '../app.css';
+    import '../app.css';
+    import { LayoutPanelLeft, TreeDeciduous, Settings } from 'lucide-svelte';
+    import { page } from '$app/stores';
+
+    let path;
+    $: path = $page.url.pathname.split('/').pop();
 </script>
 
-<slot></slot>
+<header class="fixed bottom-0 left-0 w-full h-[80px] bg-primary">
+	<nav class="flex justify-around items-center h-full">
+		<a href="/home" class="text-white {path === 'home' ? 'active' : ''}">
+			<LayoutPanelLeft class="w-8 h-8" />
+		</a>
+		<a href="/tree" class="text-white {path === 'tree' ? 'active' : ''}">
+			<TreeDeciduous class="w-8 h-8" />
+		</a>
+		<a href="/settings" class="text-white {path === 'settings' ? 'active' : ''}">
+			<Settings class="w-8 h-8" />
+		</a>
+	</nav>
+</header>
+<slot />
+
+<style>
+	nav a {
+		@apply p-4 rounded-full shadow-lg;
+		transition: all 0.3s;
+	}
+
+	nav a.active {
+		@apply bg-white text-primary mb-20;
+	}
+</style>
